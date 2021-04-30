d4524bda43be2b87ffadf0344bb745e3977d1cea
splans <splans@ackcent.com>
2021-04-30 16:06:48 +0200

    commands working

--- first_enum.go
+++ first_enum.go
@@ -1,46 +1,50 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "os/exec"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 type machine struct {
-    ip string
-    nmap_done bool
-    gobuster_instances int
-    gobuster_done bool
+	ip                 string
+	nmap_done          bool
+	gobuster_instances int
+	gobuster_done      bool
 }
 
-func launch_scan(machine machine){
-    exec.Command("mkdir " + machine.ip)
-    exec.Command("nmap -sC -sV -O -p- -oA " + machine.ip + "/nmap " + machine.ip)
-    machine.gobuster_done = true
-    exec.Command("grep 'http' nmap.gmap")
-
+func launch_scan(machine machine) {
+	fmt.Println(exec.Command("mkdir", machine.ip).Run())
+	fmt.Println(exec.Command("nmap", "-sC", "-sV", "-O", "-p-", "-oA", machine.ip+"/nmap ", machine.ip).Output())
+	machine.gobuster_done = true
+	fmt.Println(exec.Command("grep", "'http'", "nmap.gmap").Run())
 
 }
 func main() {
-    var current_scans [100]string :=ine, 0)
-    file, err := os.Open(os.Args[1])
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	var current_scans [100]machine
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        fmt.Println(scanner.Text())
-    //READ
-        m := machine{ip:scanner.Text(), nmap_done: false, gobuster_instances: 0, gobuster_done: false}
-        current_scans = append(m)
-        go launch_scan(m)
-    }
+	scanner := bufio.NewScanner(file)
+	var i = 0
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		//READ
+		m := machine{ip: scanner.Text(), nmap_done: false, gobuster_instances: 0, gobuster_done: false}
+		current_scans[i] = m
+		fmt.Println(m)
+		i = i + 1
+		go launch_scan(m)
+	}
+	for {
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
